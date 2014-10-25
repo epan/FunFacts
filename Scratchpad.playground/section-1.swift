cc9b855c1bf4cf17eb3c15bd9cda7ade5abab4ba
epan <ericpan@gmail.com>
2014-10-25 13:48:34 -0700

    Adds playground and action

---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Swift Recap
+
+var someVariable = "a variable"
+let someConstant = 20
+
+someVariable = "another string"
+// someConstant = 15 //fail
