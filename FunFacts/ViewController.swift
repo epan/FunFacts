4aee98879a9f9b81c2a6b91ee0b3526b10de98c9
epan <ericpan@gmail.com>
2014-10-25 14:03:46 -0700

    Uses facts array

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
     
+    let factsArray = ["Ants stretch when they wake up in the morning.", "Ostritches can run faster than horses."]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factsArray[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +27,8 @@ class ViewController: UIViewController {
 
     @IBAction func showFact() {
         // println("You pressed me!")
-        funFactLabel.text = "Another interesting fact"
+
+        funFactLabel.text = factsArray[1]
     }
 
 }
