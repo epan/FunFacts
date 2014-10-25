c995f92cd64695fe7283e12f21bc8c6d0a340537
epan <ericpan@gmail.com>
2014-10-25 14:11:55 -0700

    Applies struct for facts

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
     
-    let factsArray = ["Ants stretch when they wake up in the morning.", "Ostritches can run faster than horses."]
+    let factBook = FactBook()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        funFactLabel.text = factsArray[0]
+        funFactLabel.text = factBook.factsArray[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     @IBAction func showFact() {
         // println("You pressed me!")
 
-        funFactLabel.text = factsArray[1]
+        funFactLabel.text = factBook.factsArray[1]
     }
 
 }
